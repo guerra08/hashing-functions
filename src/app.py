786abd5c1235e8b2505ad528edf0a906bb5c6cd2
Bruno Guerra <gguerrabruno@outlook.com>
2021-10-03 10:34:11 -0300

    Adds helper messages

--- src/app.py
+++ src/app.py
@@ -3,7 +3,11 @@ from utils.file_reader import read_file
 from core.process import process_file
 
 def main():
-    file_bytes = read_file(sys.argv[1])
-    h0 = process_file(file_bytes)
-    print("H0: " + h0)
-main();
+    if len(sys.argv) > 1:
+        file_bytes = read_file(sys.argv[1])
+        h0 = process_file(file_bytes)
+        print("H0: " + h0)
+    else:
+        print("Bruno Guerra - Hash functions: H0 computing")
+        print("Please inform the name of the file.")
+main()
