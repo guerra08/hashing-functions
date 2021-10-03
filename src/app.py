541a0aa61c67a07efec03c6c1cce703767a2b9d2
Bruno Guerra <gguerrabruno@outlook.com>
2021-10-02 21:21:06 -0300

    File name from argv

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
-import os
+import sys
 from utils.file_reader import read_file
 from core.process import process_file
 
 def main():
-    dirname = os.path.dirname(__file__)
-    file_bytes = read_file(dirname +  "/" + "resources/FuncoesResumo - SHA1.mp4")
+    file_bytes = read_file(sys.argv[1])
     h0 = process_file(file_bytes)
-    print(h0)
+    print("H0: " + h0)
 main();
